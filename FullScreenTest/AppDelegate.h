9de069c726a0539ebd2eb61300d4552da69139b0
Alexander von Below <Alex@vonBelow.Com>
2013-06-15 00:22:03 +0200

    Initial Commit

---
+++ FullScreenTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FullScreenTest
+//
+//  Created by Alexander v. Below on 15.06.13.
+//  Copyright (c) 2013 Alexander v. Below. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FullScreenTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FullScreenTest
+//
+//  Created by Alexander v. Below on 15.06.13.
+//  Copyright (c) 2013 Alexander v. Below. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
